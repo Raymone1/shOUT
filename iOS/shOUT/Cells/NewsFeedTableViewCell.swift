dc8bbde3bdcad0c2826fc6a5e6826a1af5ead74e
Raymone Radi <Raymone@dhcp-rhodes-1499.eduroam.cornell.edu>
2017-12-01 16:01:22 -0500

    newsfeedtableviewcell setup

--- iOS/shOUT/Cells/NewsFeedTableViewCell.swift
+++ iOS/shOUT/Cells/NewsFeedTableViewCell.swift
@@ -14,17 +14,17 @@ class NewsFeedTableViewCell2: UITableViewCell {
     var delegate: //fill me in
     
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        selectionStyle = .default
-        
-        dateText = UILabel()
-        
-        dateText.textColor = UIColor.white.withAlphaComponent(0.8)
-        
-        addSubview(dateText)
+    override func awakeFromNib() {
+    super.awakeFromNib()
+    selectionStyle = .default
+    
+    dateText = UILabel()
+    
+    dateText.textColor = UIColor.white.withAlphaComponent(0.8)
+    
+    addSubview(dateText)
     }
+
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
