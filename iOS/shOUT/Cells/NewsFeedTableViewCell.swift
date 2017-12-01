cc3167ad801b086f96d82ad48aa4a30e4d99ddb2
Raymone Radi <Raymone@dhcp-rhodes-1499.eduroam.cornell.edu>
2017-12-01 15:50:30 -0500

    newsfeedtableviewcell setup

---
+++ iOS/shOUT/Cells/NewsFeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsFeedTableViewCell.swift
+//  shOUT
+//
+//  Created by Raymone Radi  on 12/1/17.
+//  Copyright © 2017 shOUT. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
